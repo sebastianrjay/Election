2bb6e67e0ddefae32ec39384757294243bc169b2
Sebastian Jay <sebastianrjay@gmail.com>
2016-03-25 23:16:18 -0700

    Minify JS assets with Grunt; add TV background img

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+	    my_target: {
+	      files: {
+	        'app.js': [
+	        	'lib/constants.js',
+            'lib/util.js',
+            'lib/falling-object.js',
+            'lib/hand.js',
+            'lib/bill.js',
+            'lib/newspaper.js',
+            'lib/start-view.js',
+            'lib/game-view.js',
+            'lib/finish-view.js',
+            'lib/game.js',
+            'lib/preloader.js'
+	        ]
+	      }
+	    }
+	  }
+  });
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+};
